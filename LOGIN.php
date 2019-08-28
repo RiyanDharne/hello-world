15ad6b3cd8df9790b0c91a75dfb0c86d62a7b20d
RiyanDharne <54630323+RiyanDharne@users.noreply.github.com>
2019-08-28 18:43:02 +0530

    Add files via upload
    help me make this website better

---
+++ LOGIN.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<style>
+.bdy{
+background-color:Turquoise;
+}
+</style>
+<body class="bdy">
+<div id="frm" align = "center">
+<style>
+.jumbotron{
+    background-color:PaleTurquoise;
+
+    height:350px;
+    width:350px;
+    
+    }
+</style>
+<div class="jumbotron" >
+<style>
+
+</style>
+<div align="center">
+	<form action='PROCESS.php' id="form" method='post' >
+    <div align="center">
+		<p>
+			<label>
+				Username:
+			</label>
+			<input type="text" id="username" name="username">
+		</p>
+		<p>
+			<label>
+				Password:
+			</label>
+			<input type="text" id="password" name="password">
+		</p>
+      
+		<p>
+        <style>
+        .btn{width:300px;}
+        </style>
+			<button type="submit" id="btn" value="login" class = "btn btn-success btn-xs">Login</button>
+		</p>
+        </div>
+	</form>
+</div>
+</body>
+</html>
---
+++ PROCESS.php
@@ -0,0 +1,30 @@
+<?PHP
+// to get values passe from form in login.php file
+$url = 'http://localhost/Login_System/PAGE2.html';
+
+ $username = "";
+
+if(isset($_POST['username'])){
+    $username = $_POST['username'];
+}
+ $password = "";
+if(isset($_POST['username'])){
+    $password = $_POST['password'];
+ }
+ 
+ 
+ //connect to the server select database
+ mysqli_connect("localhost", "root", "","login");
+$conn=mysqli_connect("localhost", "root", "","login");
+
+ // Query the database for user
+ $result = mysqli_query($conn,"select * from users where username = '$username' and password = '$password'")
+  or die('Failed to query database');
+  $sql1="select * from users where username = '$username' and password = '$password'";
+ $row = mysqli_fetch_array($result);
+ if ( $row['username'] == $username && $row['password'] == $password ) {
+  header("Location: $url");
+ } else {
+     echo "Failed to login!";
+}
+?>
